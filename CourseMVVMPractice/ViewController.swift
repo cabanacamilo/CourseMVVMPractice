b99351c07eba317df87a9415598f6a206ca769e4
camilo cabana <camilo_11_04@hotmail.com>
2020-07-17 15:24:55 +0900

    Initial Commit

---
+++ CourseMVVMPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseMVVMPractice
+//
+//  Created by Camilo Cabana on 17/07/20.
+//  Copyright © 2020 Camilo Cabana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
